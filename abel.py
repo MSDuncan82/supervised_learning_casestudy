9caa95ede1aa6fa6fff10591eb10e1a279f132ff
Abel Desta <abeld@ksu.edu>
2020-01-24 10:03:37 -0700

    update

--- abel.py
+++ abel.py
@@ -7,12 +7,13 @@ from sklearn.linear_model import LogisticRegression as LR
 from sklearn.metrics import confusion_matrix
 from sklearn.model_selection import train_test_split
 from sklearn.neighbors import KNeighborsClassifier
+import matplotlib.pyplot as plt 
 
 
 def clean(df):
     df['signup_date'] = pd.to_datetime(df['signup_date'])
     df['last_trip_date'] = pd.to_datetime(df['last_trip_date'])
-    df['active_user'] = churn['last_trip_date'].apply(lambda x: True if x.month >= 6 else False)
+    df['active_user'] = churn['last_trip_date'].apply(lambda x: 1 if x.month >= 6 else 0)
     start = np.array([x.month for x in df['signup_date']])
     end = np.array([x.month for x in df['last_trip_date']])
     df['months_as_user'] = end - start
@@ -32,6 +33,8 @@ if __name__ == "__main__":
     # churn['months_as_user'] =churn['last_trip_date'].month - churn['signup_date'].month 
 
     df = clean(churn)
+
+    
     cols = df.columns.drop(['phone','city','signup_date', 'last_trip_date', 'months_as_user'])
     churns = df[cols]
     y = churns.pop('active_user').values  
@@ -39,45 +42,4 @@ if __name__ == "__main__":
 
     X_train, X_test, y_train, y_test =train_test_split(X, y, random_state = 1)
 
-    rf = RF(n_estimators = 100)
-    rf.fit(X_train, y_train)
-    y_pred = rf.predict(X_test)
-
-    acc = accuracy_score(y_test, y_pred)
-    confuse = confusion_matrix(y_test, y_pred)
-    print(acc)
-    print(confuse)
-
-    order = np.argsort(rf.feature_importances_) 
-    column = X_train.columns[order][::-1]
-
-    X1_train = X_train[column[1:]]
-    X1_test = X_test[column[1:]]
     
-    
-    rf1 = RF(n_estimators = 300, oob_score=True)
-    rf1.fit(X_train, y_train)
-    y1_pred = rf1.predict(X_test)
-
-    acc1 = accuracy_score(y_test, y1_pred)
-    confuse1 = confusion_matrix(y_test, y1_pred)
-    print('----------')
-    print(acc1)
-    print(confuse1)
-    
-    cols = df.columns.drop(['phone','city','signup_date', 'last_trip_date'])
-    churns = df[cols]
-    y = churns.pop('active_user').values  
-    X = churns
-    X_train, X_test, y_train, y_test =train_test_split(X, y, random_state = 1)
-
-    logit = LR(sovler = 'lbfgs')
-    logit.fit(X_train, y_train)
-    y2_pred = logit.predict(X_test)
-
-    print(logit.score(X_test,y_test))
-
-    cols = df.columns.drop(['phone','city','signup_date', 'last_trip_date'])
-
-
-
