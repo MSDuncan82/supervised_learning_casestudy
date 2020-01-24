9ce2068398572a2b2529a77db24c07b0d452502d
Abel Desta <abeld@ksu.edu>
2020-01-24 11:37:05 -0700

    update clean to clean_df

--- src/randomforest.py
+++ src/randomforest.py
@@ -7,11 +7,13 @@ from sklearn.model_selection import train_test_split
 from sklearn.neighbors import KNeighborsClassifier
 import numpy as np 
 import pandas as pd
-from clean import clean, X_y
+from clean import clean_df, X_y
 
 if __name__ == "__main__":
     churn = pd.read_csv('data/churn_train.csv')
-    df = clean(churn)
+    
+    df = clean_df(churn)
+    
     X_train, X_test, y_train, y_test = X_y(df)
     rf = RF(n_estimators=200, max_depth = 8)
     rf.fit(X_train, y_train)
